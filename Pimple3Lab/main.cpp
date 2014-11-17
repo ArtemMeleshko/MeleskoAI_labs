2bf4988ed8739e0c0246c283b2e346cc5015a5ae
ArtemMeleshko <artem50180@mail.ru>
2014-11-18 00:11:32 +0300

    Create main.cpp

---
+++ Pimple3Lab/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "Stack.h"
+
+using namespace std;
+
+int main()
+{
+	Stack s;
+	for (int i=0; i<10; i++) s.push(65+i);
+	s.show();
+
+	cout << "\npulled:\n";
+	for (int i=0; i<3; i++) cout << s.pull();
+	cout << endl;
+
+	s.show();
+
+	cout << "\npulled:\n";
+	for (int i=0; i<2; i++) cout << s.pull();
+	cout << endl;
+
+	s.show();
+
+	return 0;
+}
