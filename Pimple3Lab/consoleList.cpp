f7a65ab0dc3d584b6acd7db9d616dea2fec8129a
ArtemMeleshko <artem50180@mail.ru>
2014-11-18 00:14:57 +0300

    Create consoleList.cpp

---
+++ Pimple3Lab/consoleList.cpp
@@ -0,0 +1,52 @@
+#include "consoleList.h"
+#include <iostream>
+
+
+using namespace std;
+
+consoleList::consoleList():
+    List()
+{}
+consoleList()::ConsoleList(const ConsoleList &other):
+    List(other)
+{}
+consoleList::consoleList(consoleList &&other)noexcept:
+List (other)
+{}
+
+consoleList &consoleList::operator=(const consoleList &other)
+{
+    return *this;
+}
+consoleList &consoleList::operator=(const consoleList &other)noexcept
+{
+    return *this;
+}
+
+consoleList::~consoleList() noexcept
+{}
+
+void consoleList::output()const
+{
+    PositionedData *array = nullptr;
+    int size = 0;
+    getElements(array, size);
+    printElements(array, size);
+    delete [] array;
+    array = nullptr;
+}
+
+void consoleList::consoleList(PositionedData *array, int size)const
+{
+    for(int i = 0; i < size; ++i)
+    {
+        if(array[i].position == PositionedData::head)
+            cout << "(" << array[i].datum << ")";
+        else
+            cout << array[i].datum;
+
+        cout << " ";
+    }
+
+    cout << endl;
+}
