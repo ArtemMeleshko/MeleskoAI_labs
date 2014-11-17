940109d4aff640fc4c0ed4160382831b18b75102
ArtemMeleshko <artem50180@mail.ru>
2014-11-18 00:10:59 +0300

    perivate /...

---
+++ Pimple3Lab/implimentation.h
@@ -0,0 +1,14 @@
+#ifndef IMPLIMENTATION_H_INCLUDED
+#define IMPLIMENTATION_H_INCLUDED
+#include "List.h"
+class List::implimentation
+{
+public:
+    void add;
+    void show;
+    void put(char c, int pos);
+    chat get(int pos);
+    bool del;
+private:
+    ...
+};
