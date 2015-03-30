273b276b40a81b05c79f64fecb918fcd8afb567c
ArtemMeleshko <artem50180@mail.ru>
2015-03-30 22:17:49 +0300

    Create NumberColories.java

---
+++ Aa.JAVA/lab-3/regular/NumberColories.java
@@ -0,0 +1,55 @@
+
+import java.io.Serializable;
+
+public  interface  NumberColories  {
+	double Calories();
+	public void Norma(double goal , double typelife);
+	
+}
+	
+@SuppressWarnings("serial")
+class Woman implements  Serializable
+{
+		double weight, height;
+		int age;
+		Woman ( double weight, double height, int age){
+			this.weight=weight;
+			this.height=height;
+			this.age=age;
+			
+		}
+		public double Calories(){
+			return 477.6+(9.2*weight)+(3.1*height)-(4.3*age);
+		}
+		
+	
+		
+		public void Norma(double typelife, double goal){
+			System.out.println("Суточная норма каллорий " + Math.rint(this.Calories()*typelife)* goal);
+			System.out.println("Белков " + Math.rint(((((this.Calories()*typelife)* goal)*0.4)/4)));
+			System.out.println("Жиров " + Math.rint(((((this.Calories()*typelife)* goal)*0.2)/9)));
+			System.out.println("Углеводов " + Math.rint(((((this.Calories()*typelife)* goal)*0.4)/4)));
+		}
+}
+	
+
+
+@SuppressWarnings("serial")
+class Man implements Serializable {
+	double weight, height;
+	int age;
+	Man ( double weight, double height, int age){
+		this.weight=weight;
+		this.height=height;
+		this.age=age;
+	}
+	public double Calories(){
+		return 88.36+(13.4*weight)+(4.8*height)-(5.7*age);
+	}
+	public void Norma(double typelife, double goal){
+		System.out.println("Суточная норма каллорий " + Math.rint(this.Calories()*typelife)* goal);
+		System.out.println("Белков " + Math.rint(((((this.Calories()*typelife)* goal)*0.4)/4)));
+		System.out.println("Жиров " + Math.rint(((((this.Calories()*typelife)* goal)*0.2)/9)));
+		System.out.println("Углеводов " + Math.rint(((((this.Calories()*typelife)* goal)*0.4)/4)));
+	}
+}
