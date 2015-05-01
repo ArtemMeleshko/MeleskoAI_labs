119eb4aa0e4580accfd23568e1c67aa32cc17eb1
ArtemMeleshko <artem50180@mail.ru>
2015-05-01 08:55:42 +0300

    Update PolTest.java

--- Aa.JAVA/Lab4/PolTest.java
+++ Aa.JAVA/Lab4/PolTest.java
@@ -10,13 +10,13 @@ public class PolTest {
 	public void testCalories() throws Exception
 	{
 		
-		Pol Cal0 = new Woman(60 , 160 , 20);
-	    double n =  Cal0.Calories();
-	    assertEquals(1439.6 , n);
+             Pol Cal0 = new Woman(60 , 160 , 20);
+	        double n =  Cal0.Calories();
+	        assertEquals(1439.6 , n);
 		
 		
 		
-		Pol Cal1 = new Man(80 , 180 , 20);
+	     Pol Cal1 = new Man(80 , 180 , 20);
 		 double m = Cal1.Calories();
 		 assertEquals(1905.36 , n);
 	}
@@ -26,11 +26,11 @@ public class PolTest {
 	@Test
 	public void testCalories1() throws Exception 
 	{
-		Pol Calories0 = new Woman(60, 160, 20);
+	       Pol Calories0 = new Woman(60, 160, 20);
 		 double n = Calories0.Calories1(1.0 , 1.2);
 		 assertEquals(1727.52 , n);
 		
-		Pol Calories01 = new Man(80, 180, 20);
+	       Pol Calories01 = new Man(80, 180, 20);
 		 double m = Calories01.Calories1(1.0 , 1.5);
 		 assertEquals(2858.04 , m);
 	}
